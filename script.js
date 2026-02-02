418fd78d4fb99d0458256bb1a58f3b62612dc2a5
David_Kamau <davidkm.official@gmail.com>
2026-02-02 21:53:18 +0300

    List for akan indexes

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const monthInput = document.getElementById("Month")
 const year1 = document.getElementById("Year1")
 const year2 = document.getElementById("Year2")
 const gender = document.getElementById("Gender")
+
+const indexAkan = {
+  Sunday : 0,
+  Monday : 1,
+  Tuesday : 2,
+  Wednesday : 3,
+  Thursday : 4,
+  Friday : 5,
+  Saturday : 6
+};
+
