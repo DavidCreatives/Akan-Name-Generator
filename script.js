38e661899cbcda9253750d3c5fa324581e141061
David_Kamau <davidkm.official@gmail.com>
2026-02-02 21:48:18 +0300

    Defining variables for the objects in Js

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM elements
+const dateInput = document.getElementById("Date");
+const monthInput = document.getElementById("Month")
+const year1 = document.getElementById("Year1")
+const year2 = document.getElementById("Year2")
+const gender = document.getElementById("Gender")
