97ebc29adf772b652d2aae2a51fbb2deab76fcd0
David_Kamau <davidkm.official@gmail.com>
2026-02-03 10:08:59 +0300

    Working prototype with reloading error

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const gender = document.getElementById("Gender").value;
 const Result = document.getElementById("ResultW");
 
 function akanName(){
-  const DD = Number(dateInput)
-  const MM = Number(monthInput)
+  let DD = Number(dateInput)
+  let MM = Number(monthInput)
 
-  const CC = Number(Math.floor(year / 100))
-  const YY = Number((year % 100))
+  let CC = Number(year.substring(0, 2))
+  let YY = Number(year.substring(2, 4))
 
   const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "THursday", "Friday", "Saturday"]
 
@@ -18,13 +18,14 @@ function akanName(){
   const femaleAkan = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
   let d = Math.floor((((4 * CC - 2) * (CC - 1)) + (45 * YY) + (1026 * (MM + 1)) + DD) % 7)
-    d = Math.abs(d);
+  d = Math.abs(d);
+
   if(gender === "Male") {
     let Name = maleAkan[d]
-    Result.textContent = `Being a ${gender} born on ${DD}/${MM}/${CC}${YY}, your Akan name is ${Name}`
+    Result.textContent = `Being a ${gender} born on ${day[d]} in the year ${year}, your Akan name is ${Name}`
   } else {
     let Name = femaleAkan[d]
-    Result.textContent = `Being a ${gender} born on ${DD}/${MM}/${CC}${YY}, your Akan name is ${Name}`
+    Result.textContent = `Being a ${gender} born on ${day[d]} in the year ${year}, your Akan name is ${Name}`
   };
 }
 
